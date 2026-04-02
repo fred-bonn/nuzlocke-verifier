958edfefa732fef5c93551cef7e91035d52bfa01
fred-bonn <sadpantz@gmail.com>
2026-04-02 13:51:57 +0200

    added parser structs file, and parse error

--- internal/parser/lexer_error.go
+++ internal/parser/lexer_error.go
@@ -8,15 +8,6 @@ type lexError struct {
 	Char   byte
 }
 
-type parseError struct {
-	Previous token
-	Current  token
-}
-
 func (e lexError) Error() string {
 	return fmt.Sprintf("error: lexer failed at %d:%d (illegal character: %q)", e.Line, e.Column, e.Char)
-}
-
-func (e parseError) Error() string {
-	return fmt.Sprintf("error: parser failed: did not expect token %s after %s", e.Current, e.Previous)
 }
